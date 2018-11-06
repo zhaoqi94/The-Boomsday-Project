9739b6f3cf92942a26be62c9abe73f8dcaa93c7d
zhaoqi94 <734038404@qq.com>
2018-11-06 13:31:52 +0800

    complete staged function & add plot

--- ensemble/adaboost.py
+++ ensemble/adaboost.py
@@ -45,13 +45,28 @@ class AdaBoostClassifier:
             dcf_i = (self.estimators[i].predict(X) == self.classes[:, np.newaxis]).T * self.alpha[i]
             total_dcf.append(dcf_i)
         sum_dcf = np.sum(total_dcf, axis=0)
+        sum_dcf /= np.sum(self.alpha)
         return sum_dcf
 
+    # 输出前i个分类器的组合，可用于显示集成的效果
+    # 以及做模型选择
     def staged_decision_function(self, X):
-        pass
+        dcf = None
+        norm = 0
+        for i in range(self.n_estimators):
+            current_dcf = (self.estimators[i].predict(X) == self.classes[:, np.newaxis]).T * self.alpha[i]
+            norm += self.alpha[i]
+            if dcf is None:
+                dcf = current_dcf
+            else:
+                dcf += current_dcf
+
+            yield dcf / norm
 
     def staged_predict(self, X):
-        pass
+        for dcf in self.staged_decision_function(X):
+            yield self.classes[np.argmax(dcf, axis=1)]
 
     def staged_score(self, X, y):
-        pass
+        for y_pred in self.staged_predict(X):
+            yield np.mean(y_pred==y)
--- ensemble/test_adaboost.py
+++ ensemble/test_adaboost.py
@@ -3,16 +3,36 @@ from sklearn.tree import DecisionTreeClassifier
 from ensemble.adaboost import AdaBoostClassifier
 # from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 X, y = make_hastie_10_2(n_samples=12000, random_state=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 # clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), n_estimators=500, algorithm="SAMME")
-clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), n_estimators=3000)
+clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), n_estimators=5000)
 
 print(X_train.shape)
 print(X_test.shape)
-
+#
 clf.fit(X_train, y_train)
-print(clf.score(X_train, y_train))
-print(clf.score(X_test, y_test))
+print("Training Accuracy: %0.4f" % clf.score(X_train, y_train))
+print("Testing Accuracy: %0.4f" % clf.score(X_test, y_test))
+
+# print(clf.decision_function(X_test[0:3]))
+for x in clf.staged_decision_function(X_test[0:3]):
+    # print(x)
+    pass
+
+
+# collect error rate
+error_rate = []
+for err in clf.staged_score(X_test, y_test):
+    # print(x)
+    error_rate.append(1-err)
+
+
+# plot error rate
+x_cord = list(range(len(error_rate)))
+plt.plot(x_cord, error_rate)
+plt.show()
+
